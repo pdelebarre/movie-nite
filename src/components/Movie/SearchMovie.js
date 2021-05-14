14293a9fd9281b363d0bb05abc2e9b1c11c110dc
pdelebarre <pdelebarre@gmail.com>
2021-05-14 20:41:27 +0200

    small fix

--- src/components/Movie/SearchMovie.js
+++ src/components/Movie/SearchMovie.js
@@ -5,27 +5,28 @@ import classes from "./SearchMovie.module.css";
 export default function SearchMovie(props) {
   const [query, setQuery] = useState();
   const [results, setResults] = useState([]);
-  const [isVisible, setIsVisible] = useState(false);
+ // const [isVisible, setIsVisible] = useState(false);
 
-  const searchMovies = async (e) => {
-    e.preventDefault();
-    console.log("submitting");
+  // const searchMovies = async (e) => {
+  //   e.preventDefault();
+  //   console.log("submitting");
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+  //   const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
 
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data);
-      setResults(data.results);
-      setIsVisible(true);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  //   try {
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+  //     console.log(data);
+  //     setResults(data.results);
+  //     //setIsVisible(true);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(async () => {
+      //e.preventDefault();
       console.log(query);
       const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
       try{
@@ -36,12 +37,12 @@ export default function SearchMovie(props) {
         console.log(data);
         // setIsLoaded(true);
         setResults(data.results);
-        setIsVisible(true);
+        //setIsVisible(true);
       }
         catch (err) {
           console.error(err);
         }
-    }, [5000, isVisible]);
+    }, [1000]);
 
     return () => clearTimeout(delayDebounceFn);
   }, [query]);
@@ -51,13 +52,13 @@ export default function SearchMovie(props) {
   };
 
   const onSelectHandler = (movie) => {
-    setIsVisible(query.length() > 1);
+    //setIsVisible(query.length() > 1);
     return props.onSelect(movie);
   };
 
   return (
     <>
-      <form className={classes["search-form"]} onSubmit={searchMovies}>
+      <form className={classes["search-form"]}>
         <label className={classes["search-label"]} htmlFor="query">
           Movie Name
         </label>
