f319fef38540e33809cfe476a9342105185e1d9a
pdelebarre <pdelebarre@gmail.com>
2021-05-14 20:32:18 +0200

    search as the user types

--- src/components/Movie/Overview.js
+++ src/components/Movie/Overview.js
@@ -1,6 +1,20 @@
-import classes from './Overview.module.css';
+import classes from "./Overview.module.css";
 const Overview = (props) => {
-    return (<div className={classes.overview}>{props.movie.overview}</div>)
+  return (
+    <div className={classes["grid-container"]}>
+      <div className={classes.title}>{props.movie.title}</div>
+      <div>
+          
+        <img
+          className={classes.poster}
+          src={`https://image.tmdb.org/t/p/original${props.movie.poster_path}`}
+          align="left"
+          alt="poster"
+        />
+        <div className={classes.overview}>{props.movie.overview}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Overview;
--- src/components/Movie/SearchMovie.js
+++ src/components/Movie/SearchMovie.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Results from "../Search/Results";
-import classes from './SearchMovie.module.css';
+import classes from "./SearchMovie.module.css";
 
 export default function SearchMovie(props) {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState();
   const [results, setResults] = useState([]);
   const [isVisible, setIsVisible] = useState(false);
 
@@ -23,12 +23,35 @@ export default function SearchMovie(props) {
       console.error(err);
     }
   };
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(async () => {
+      console.log(query);
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+      try{
+
+        
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+        // setIsLoaded(true);
+        setResults(data.results);
+        setIsVisible(true);
+      }
+        catch (err) {
+          console.error(err);
+        }
+    }, [5000, isVisible]);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [query]);
+
   const onChangeHandler = (event) => {
     setQuery(event.target.value);
   };
 
   const onSelectHandler = (movie) => {
-    setIsVisible((st) => !st);
+    setIsVisible(query.length() > 1);
     return props.onSelect(movie);
   };
 
@@ -46,13 +69,13 @@ export default function SearchMovie(props) {
           onChange={onChangeHandler}
           value={query}
         />
-        {query.length > 1 && (
-          <button className={classes['search-button']} type="submit">
+        {/* {query.length > 0 && (
+          <button className={classes["search-button"]} type="submit">
             Search
           </button>
-        )}
+        )} */}
       </form>
-      {isVisible && <Results results={results} onSelect={onSelectHandler} />}
+      {results != null && <Results results={results} onSelect={onSelectHandler} />}
     </>
   );
 }
